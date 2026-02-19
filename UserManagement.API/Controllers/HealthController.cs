89eda32c3d8704324b81ec60bd9c3655523d0353
Charlie Briones <charliebriones@gmail.com>
2026-02-19 19:06:13 +0800

    added health for cron.job

---
+++ UserManagement.API/Controllers/HealthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagement.API.Controllers
+{
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet("/health")]
+        public IActionResult Health()
+            => Ok(new { status = "ok" });
+    }
+}
